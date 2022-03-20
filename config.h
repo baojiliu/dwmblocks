accbaa8faff500a1ffd5450d34b3713b1fb5e26e
baoji.liu <baoji.liu@gmail.com>
2022-03-20 19:29:03 +0800

    :pencil:

--- config.h
+++ config.h
@@ -19,12 +19,12 @@ static const Block blocks[] = {
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	{"",	"sb-forecast",	18000,	5},
 	{"",	"sb-mailbox",	180,	12},
-	{"",	"sb-nettraf",	1,	16},
+	//{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
-	{"",	"sb-help-icon",	0,	15},
+	//{"",	"sb-help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
